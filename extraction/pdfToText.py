c027e7cd49326e50c6657f3136a2efe6d84d58e6
Sagnik Anupam <sagnikanupam@gmail.com>
2023-03-19 13:22:21 -0400

    single word lines removed

--- extraction/pdfToText.py
+++ extraction/pdfToText.py
@@ -3,7 +3,18 @@ import pdftotext
 # Load your PDF
 with open("example.pdf", "rb") as f:
     pdf = pdftotext.PDF(f)
-print(len(pdf)) #number of pages
-for page in pdf:
-    print(page)
-print("\n\n".join(pdf)) #join all strings
+# print(len(pdf)) #number of pages
+# for page in pdf:
+#    print(page)
+#
+s = "\n\n".join(pdf) #join all strings
+split = s.split('\n')
+corrected_string = []
+for line in split:
+    if len(line.split(' '))>1:
+        corrected_string.append(line)
+final_text = "\n".join(corrected_string)
+print(final_text)
+#print(s)
+
+
