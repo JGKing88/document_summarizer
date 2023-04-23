aca38875a60d470893c3f353f68ee9de0b715bfe
Sagnik Anupam <sagnikanupam@gmail.com>
2023-04-23 11:31:47 -0400

    "Fix api_key"

--- ds/app.py
+++ ds/app.py
@@ -5,13 +5,14 @@ from io import BytesIO
 # import pdftotext
 from transformers import GPTJForCausalLM, AutoTokenizer
 import torch 
-
+import pdftotext
 import os
 import openai
 from dotenv import load_dotenv
+import ds_config
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = Config.api_key() #os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 CORS(app)
@@ -34,42 +35,42 @@ def upload_pdf():
 def process_pdf_and_user_information(uploaded_file, user_information="", summary_details=""):
     # Placeholder function for processing the PDF and user_information
     # You can implement your own summary generation logic here
-    # pdf = pdftotext.PDF(uploaded_file)
-    # s = "\n\n".join(pdf) #join all strings
-    # split = s.split('\n')
-    # corrected_string = []
-    # for line in split:
-    #     if len(line.split(' '))>1:
-    #         corrected_string.append(line)
-    # final_text = "\n".join(corrected_string)
-    # #print(final_text)
-    # #print(s)
+    pdf = pdftotext.PDF(uploaded_file)
+    s = "\n\n".join(pdf) #join all strings
+    split = s.split('\n')
+    corrected_string = []
+    for line in split:
+        if len(line.split(' '))>1:
+            corrected_string.append(line)
+    document = "\n".join(corrected_string)
+    #print(final_text)
+    #print(s)
 
-    # document = ""
+    #document = ""
 
-    # prompt_prompt = "Generate a prompt that tells ChatGPT to generate a summary of the following document for this type of user: " + user_information + " and this type of summary: " + summary_details
-    # prompt = openai.ChatCompletion.create(
-    #     model="gpt-4",
-    #     messages=[
-    #             {"role": "user", "content": prompt_prompt},
-    #         ],
-    #     temperature=0.2, #[0,2] higher values are more random (want low randomnes)
-    #     max_tokens=64,
-    # )
+    prompt_prompt = "Generate a prompt that tells ChatGPT to generate a summary of the following document for this type of user: " + user_information + " and this type of summary: " + summary_details
+    prompt = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+                {"role": "user", "content": prompt_prompt},
+            ],
+        temperature=0.2, #[0,2] higher values are more random (want low randomnes)
+        max_tokens=64,
+    )
 
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-4",
-    #     messages=[
-    #             {"role": "user", "content": prompt.choices[0].message.content + "\n" + document},
-    #         ],
-    #     temperature=0.7, #higher values are more random
-    #     max_tokens=64,
-    #     frequency_penalty=0.0, #[-2, 2] Positive values decrease the model's likelihood to repeat the same line verbatim.
-    #     presence_penalty=-1.0 #[-2, 2] Positive values increase the model's likelihood to talk about new topics.
-    # )
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+                {"role": "user", "content": prompt.choices[0].message.content + "\n" + document},
+            ],
+        temperature=0.7, #higher values are more random
+        max_tokens=64,
+        frequency_penalty=0.0, #[-2, 2] Positive values decrease the model's likelihood to repeat the same line verbatim.
+        presence_penalty=-1.0 #[-2, 2] Positive values increase the model's likelihood to talk about new topics.
+    )
 
-    # gen_text = response.choices[0].message.content
-
+    gen_text = response.choices[0].message.content
+    return gen_text
 
     # context = user_information + summary_details
     # document = ""
@@ -85,7 +86,7 @@ def process_pdf_and_user_information(uploaded_file, user_information="", summary
     #     presence_penalty=-1.0 #[-2, 2] Positive values increase the model's likelihood to talk about new topics.
     # )
 
-    return "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus"
+    # return "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus"
 
 if __name__ == '__main__':
     app.run(debug=True)
